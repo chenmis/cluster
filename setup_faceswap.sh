ed8a96b9dc9484b7a22c488a8f0c3a643c067f08
chenmis <47525602+chenmis@users.noreply.github.com>
2020-10-24 19:31:54 +0300

    Update setup_faceswap.sh

--- setup_faceswap.sh
+++ setup_faceswap.sh
@@ -2,7 +2,7 @@
 ### sbatch config parameters must start with #SBATCH and must precede any other command. to ignore just add another # - like ##SBATCH
 
 #SBATCH --partition gtx1080                         ### specify partition name where to run a job. NVidia 2080: short - 7 days; 1080: gtx1080 - 7 days; debug – for testing - 2 hours and 1 job at a time
-#SBATCH --time 0-01:30:00                           ### limit the time of job running. Make sure it is not greater than the partition time limit!! Format: D-H:MM:SS
+#SBATCH --time 0-07:00:00                           ### limit the time of job running. Make sure it is not greater than the partition time limit!! Format: D-H:MM:SS
 #SBATCH --job-name install faceswap                 ### name of the job. replace my_job with your desired job name
 #SBATCH --output faceswap-id-%J.out                 ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=chenmis@post.bgu.ac.il          ### users email for sending job status notifications
