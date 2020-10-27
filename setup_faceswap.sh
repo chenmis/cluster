ab0fa4a09b0a009bdb7d23310c1c70e05700dbbb
chenmis <47525602+chenmis@users.noreply.github.com>
2020-10-27 15:05:01 +0200

    Update setup_faceswap.sh

--- setup_faceswap.sh
+++ setup_faceswap.sh
@@ -25,7 +25,9 @@ pip install --user opencv-python
 pip install --user pynvml
 pip install --user imageio_ffmpeg
 pip install --user fastcluster
-### python ../faceswap/faceswap.py extract -i take002.mp4 -o src002 ###
-### python ../faceswap/faceswap.py train -A src000 -B src002 -m train ###
+
+python ../faceswap/faceswap.py extract -i take000.mp4 -o src000
+python ../faceswap/faceswap.py extract -i take002.mp4 -o src002
+python ../faceswap/faceswap.py train -A src000 -B src002 -m train
 python ../faceswap/faceswap.py convert -i src000 -o converted -m train
 
