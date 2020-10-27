2452df934760b8811f3c7c933d67e9f38468805a
chenmis <47525602+chenmis@users.noreply.github.com>
2020-10-27 11:20:11 +0200

    Update setup_faceswap.sh

--- setup_faceswap.sh
+++ setup_faceswap.sh
@@ -20,4 +20,4 @@ echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
 
 module load anaconda                          ### load anaconda module
 source activate py37                          ### activating environment, environment must be configured before running the job
-python ../faceswap/setup.py
+python ../faceswap/faceswap.py extract -i take000.mp4 -o src000
