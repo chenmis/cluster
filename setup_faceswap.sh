2932514df61259aee3bf9c1bd1a60b10ef364f17
chenmis <47525602+chenmis@users.noreply.github.com>
2020-10-28 15:39:45 +0200

    Update setup_faceswap.sh

--- setup_faceswap.sh
+++ setup_faceswap.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition gtx1080                         ### specify partition name where to run a job. NVidia 2080: short - 7 days; 1080: gtx1080 - 7 days; debug – for testing - 2 hours and 1 job at a time
 #SBATCH --time 0-07:00:00                           ### limit the time of job running. Make sure it is not greater than the partition time limit!! Format: D-H:MM:SS
 #SBATCH --job-name run_faceswap                 ### name of the job. replace my_job with your desired job name
-#SBATCH --output faceswap-id-%J.out                 ### output log for running job - %J is the job number variable
+#SBATCH --output run_faceswap-id-%J.out                 ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=chenmis@post.bgu.ac.il          ### users email for sending job status notifications
 #SBATCH --mail-type=BEGIN,END,FAIL                  ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
 #SBATCH --gres=gpu:1                                ### number of GPUs (can't exceed 8 gpus for now) ask for more than 1 only if you can parallelize your code for multi GPU
@@ -20,12 +20,12 @@ echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
 
 module load anaconda                          ### load anaconda module
 source activate py37                          ### activating environment, environment must be configured before running the job
-pip install --user tensorflow==2.2
-pip install --user opencv-python
-pip install --user pynvml
-pip install --user imageio_ffmpeg
-pip install --user fastcluster
-pip install --user pathlib
+### pip install --user tensorflow==2.2 ### 
+### pip install --user opencv-python ### 
+### pip install --user pynvml ### 
+### pip install --user imageio_ffmpeg ### 
+### pip install --user fastcluster ### 
+### pip install --user pathlib ### 
 
 ### python ../faceswap/faceswap.py extract -i take000.mp4 -o src000 ###
 ### python ../faceswap/faceswap.py extract -i take002.mp4 -o src002 ###
