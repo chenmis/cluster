7641d6d74b73740a480088bab14e8f88c8bf8627
chenmis <47525602+chenmis@users.noreply.github.com>
2020-11-12 14:31:45 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 from subprocess import Popen, PIPE
 p = Popen(['../faceswap/faceswap.py train -A faceSwapProject/faceA -B faceSwapProject/faceB -m faceSwapProject/ModelAB -tia faceSwapProject/faceA -tib faceSwapProject/faceB -to faceSwapProject/TimelapseAB'], stdin=PIPE, shell=True)
-p.communicate(input='\n')
+p.communicate(input=ord("\n"))
