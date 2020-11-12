85f01b0b0614cff4e7d05ab9e5d1672922d09630
chenmis <47525602+chenmis@users.noreply.github.com>
2020-11-12 23:39:23 +0200

    Update run_deepfacelab.sh

--- run_deepfacelab.sh
+++ run_deepfacelab.sh
@@ -21,4 +21,4 @@ echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
 module load anaconda                          ### load anaconda module
 source activate deepfacelab   
 
-bash -i ../DeepFaceLab_Linux/scripts/2_extract_image_from_data_src.sh
+bash -i ../scripts/2_extract_image_from_data_src.sh
